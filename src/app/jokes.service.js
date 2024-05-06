5f7ee230b946a062f0bdcc19cc73bbdeb25a71e3
Raka DwiKS <pulangpergih@gmail.com>
2024-05-06 14:29:31 +0700

    add

---
+++ src/app/jokes.service.js
@@ -0,0 +1,16 @@
+import { Alert } from "bootstrap";
+
+export class JokesService {
+  constructor() {
+    this.tombolJokes = document.querySelector('#jokes-receh');
+    this.modalBody = document.querySelector('.modal-body');
+  }
+
+  setModal(str) {
+    this.modalBody.innerHTML = str;
+  }
+
+  onClick(cb) {
+    this.tombolJokes.addEventListener('click', cb);
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { watch } = require('fs');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: 'bundle.js',
+    },
+    watch: true,
+    devtool: false, //untuk menghilangkan function eval pada bundle.js
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+    ],
+  },
+};
